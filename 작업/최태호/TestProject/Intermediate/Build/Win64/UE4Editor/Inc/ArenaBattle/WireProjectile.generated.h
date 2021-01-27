58884255b5bf3c38588b00e335906cac59e5816b
Choi Taeho <taehodev@naver.com>
2021-01-27 21:28:00 +0900

    작업 폴더, 테스트 프로젝트

---
+++ 작업/최태호/TestProject/Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/WireProjectile.generated.h
@@ -0,0 +1,105 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class UPrimitiveComponent;
+class AActor;
+struct FVector;
+struct FHitResult;
+#ifdef ARENABATTLE_WireProjectile_generated_h
+#error "WireProjectile.generated.h already included, missing '#pragma once' in WireProjectile.h"
+#endif
+#define ARENABATTLE_WireProjectile_generated_h
+
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_SPARSE_DATA
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execOnHit);
+
+
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execOnHit);
+
+
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAWireProjectile(); \
+	friend struct Z_Construct_UClass_AWireProjectile_Statics; \
+public: \
+	DECLARE_CLASS(AWireProjectile, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle"), NO_API) \
+	DECLARE_SERIALIZER(AWireProjectile)
+
+
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAWireProjectile(); \
+	friend struct Z_Construct_UClass_AWireProjectile_Statics; \
+public: \
+	DECLARE_CLASS(AWireProjectile, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle"), NO_API) \
+	DECLARE_SERIALIZER(AWireProjectile)
+
+
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AWireProjectile(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AWireProjectile) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AWireProjectile); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWireProjectile); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AWireProjectile(AWireProjectile&&); \
+	NO_API AWireProjectile(const AWireProjectile&); \
+public:
+
+
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AWireProjectile(AWireProjectile&&); \
+	NO_API AWireProjectile(const AWireProjectile&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AWireProjectile); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWireProjectile); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AWireProjectile)
+
+
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_PRIVATE_PROPERTY_OFFSET
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_9_PROLOG
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_PRIVATE_PROPERTY_OFFSET \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_SPARSE_DATA \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_RPC_WRAPPERS \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_INCLASS \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_PRIVATE_PROPERTY_OFFSET \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_SPARSE_DATA \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_INCLASS_NO_PURE_DECLS \
+	ArenaBattle_Source_ArenaBattle_WireProjectile_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARENABATTLE_API UClass* StaticClass<class AWireProjectile>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ArenaBattle_Source_ArenaBattle_WireProjectile_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
