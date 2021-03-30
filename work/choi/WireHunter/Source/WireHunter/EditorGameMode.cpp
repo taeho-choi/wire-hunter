0acda0884fb83323a8cfd012d77f00c6eea4bc3f
조준현 <64850905+wakabass-man@users.noreply.github.com>
2021-03-30 15:44:45 +0900

    콜리전 박스 추가
    Poseable Mesh Component히트를 위한  콜리전 박스 추가

--- work/choi/WireHunter/Source/WireHunter/EditorGameMode.cpp
+++ work/choi/WireHunter/Source/WireHunter/EditorGameMode.cpp
@@ -6,7 +6,7 @@
 
 AEditorGameMode::AEditorGameMode() {
 	PlayerControllerClass = AEditorPlayerController::StaticClass();
-	DefaultPawnClass = AEditorPawn::StaticClass();
+	//DefaultPawnClass = AEditorPawn::StaticClass();
 
 	//EditorPC를 유저에게 배정하겠다는 것을 작성
 	//그 PC는 마우스로 월드와 상호작용 가능
