471591906c7d1ec78cc326c24e0f7942dac96a00
조준현 <64850905+wakabass-man@users.noreply.github.com>
2021-04-14 20:47:24 +0900

    비헤이비어 트리 연습
    .

--- work/choi/WireHunter/Source/WireHunter/WireHunterGameMode.cpp
+++ work/choi/WireHunter/Source/WireHunter/WireHunterGameMode.cpp
@@ -7,9 +7,9 @@
 AWireHunterGameMode::AWireHunterGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
-	{
+	{*/
 		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	/*}*/
 }
