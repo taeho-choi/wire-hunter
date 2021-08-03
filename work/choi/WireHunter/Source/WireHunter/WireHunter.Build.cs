03308396252130384a0f770a71cfa24b628e8815
Choi Taeho <taehodev@naver.com>
2021-08-03 23:24:17 +0900

    머테리얼 커스텀 노드, 디렉토리 설정

--- work/choi/WireHunter/Source/WireHunter/WireHunter.Build.cs
+++ work/choi/WireHunter/Source/WireHunter/WireHunter.Build.cs
@@ -8,7 +8,7 @@ public class WireHunter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "CableComponent", "Paper2D"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "CableComponent", "Paper2D", "RenderCore" });
 		PublicIncludePaths.AddRange(new string[] { "CableComponent/Public", "CableComponent/Classes", "Paper2D/Public", "Paper2D/Classes" });
 	}
 }
