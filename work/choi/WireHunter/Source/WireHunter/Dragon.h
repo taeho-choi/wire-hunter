50984ddd82db46ad24b05480762da7522b09b812
조준현 <64850905+wakabass-man@users.noreply.github.com>
2021-08-11 19:31:29 +0900

    보스 변경
    루트모션을 사용하기 위해서 보스를 Pawn -> Char 로 변경 및 콜리전 프리셋 변경

---
+++ work/choi/WireHunter/Source/WireHunter/Dragon.h
@@ -0,0 +1,125 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EngineMinimal.h"
+#include "GameFramework/Character.h"
+#include "NodeStructure.h"
+#include "WeightStructure.h"
+#include "Fireball.h"
+#include "Lightning.h"
+
+#include "Dragon.generated.h"
+
+UCLASS()
+class WIREHUNTER_API ADragon : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ADragon();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(Replicated, VisibleInstanceOnly, Category = "Status")
+		float Health;
+	UPROPERTY(BlueprintReadWrite, EditInstanceOnly, Category = "Status")
+		float MaxHealth = 10;
+
+private:
+	char Map[10][10];
+
+	FVector TargetLocation;
+	FRotator TargetRotation;
+	FStructNode Start, Goal;
+
+	TArray<FStructWeight> Min;
+	TArray<FStructNode> Closed;
+	int ScoreF[10][10];
+	int ScoreG[10][10];
+	int ScoreH[10][10];
+
+	FVector RealMap[10][10];
+
+	TArray<FStructNode> Path;
+
+	TArray<FVector> Obstacles;
+
+	bool ToFace;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<AFireball> ToSpawn;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<ALightning> ToLightning;
+
+	UPROPERTY(EditAnywhere)
+		USceneComponent* BossRoot;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable)
+		FVector FindPlayer();
+
+	void FacePlayer();
+
+	void h(FStructNode next, FStructNode end);
+
+	void g(FStructNode next, int plus);
+
+	void f(FStructNode next);
+
+	void AStar(char map[10][10], FStructNode start, FStructNode goal);
+
+	void MakeMap();
+
+	FStructNode FindTop();
+
+	int FindElement(float x, float y);
+
+	TArray<FStructNode> Regulate();
+
+	float FindDistance(FVector a, FVector b);
+
+	UFUNCTION(BlueprintCallable)
+		FVector GetGoal();
+
+	UFUNCTION(BlueprintCallable)
+		TArray<FStructNode> DoAStar();
+
+	UFUNCTION(BlueprintCallable)
+		FVector GetPath();
+
+	UFUNCTION(BlueprintCallable)
+		void Lightning();
+
+	UFUNCTION(BlueprintCallable)
+		bool GetToFace() const { return ToFace; }
+
+	UFUNCTION(BlueprintCallable)
+		void SetToFace(bool b) { ToFace = b; }
+
+	UFUNCTION(BlueprintCallable)
+		void DetectKick();
+
+	UFUNCTION(BlueprintCallable)
+		float GetHealth() const { return Health; }
+
+	void SetHealth(float value) { Health = value; }
+
+	UFUNCTION(BlueprintCallable)
+		float GetMaxHealth() const { return MaxHealth; }
+
+	void SetMaxHealth(float value) { MaxHealth = value; }
+
+	UFUNCTION(BlueprintCallable)
+		void Spawn();
+};
